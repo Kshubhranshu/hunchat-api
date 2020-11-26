f1e161354ca9cfd116ecbb028d387e686923db50
Ernesto F. González <ernestogonzalezz@yahoo.com>
2020-11-26 11:01:56 +0000

    :card_file_box: create custom User model

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Custom User model.
+    """
+    email = models.EmailField(unique=True, null=False, blank=False)
+    is_email_verified = models.BooleanField(default=False)
