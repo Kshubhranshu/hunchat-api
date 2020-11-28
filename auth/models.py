77b59b4e296bd7e5c64b7ab13a1d5f17739b049e
Ernesto F. González <ernestogonzalezz@yahoo.com>
2020-11-28 16:11:11 +0000

    :construction: update User model

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -6,5 +8,9 @@ class User(AbstractUser):
     """
     Custom User model.
     """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+
     email = models.EmailField(unique=True, null=False, blank=False)
     is_email_verified = models.BooleanField(default=False)
+
+    REQUIRED_FIELDS = ['email',]
