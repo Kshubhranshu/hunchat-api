00672e54f71dba9489537decdb60a02470f0e71f
Ernesto F. González <ernestogonzalezz@yahoo.com>
2020-12-03 21:31:40 +0000

    :sparkles: create custom storage classes

---
+++ hunchat/storage.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class S3StaticStorage(S3Boto3Storage):
+    """
+    Storage class for static files.
+    """
+    location = settings.AWS_STATIC_LOCATION
+
+
+class S3MediaStorage(S3Boto3Storage):
+    """
+    Storage class for media files.
+    """
+    location = settings.AWS_MEDIA_LOCATION
