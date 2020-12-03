cae30022bf2b754e7cac882caccda5ba3f3a5dcc
Ernesto F. González <ernestogonzalezz@yahoo.com>
2020-12-03 21:31:04 +0000

    :construction: update Video model

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.conf import settings
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Video(models.Model):
+    """
+    Video model instance.
+    """
+    description = models.CharField(blank=True, max_length=settings.VIDEO_DESCRIPTION_MAX_LENGTH)
+    file = models.FileField(upload_to='videos', blank=False, null=True,)
+    resources_link = models.URLField()
+    author = models.ForeignKey(User, related_name='videos', on_delete=models.CASCADE)
