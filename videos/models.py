0345cd1cc46683bc6eb1197ac858ea71d1b89da8
Ernesto F. González <ernestogonzalezz@yahoo.com>
2020-12-09 23:52:35 +0000

    :beers: create VideoManager

--- videos/models.py
+++ videos/models.py
@@ -4,6 +4,21 @@ from django.conf import settings
 
 
 User = get_user_model()
+VideoCommentNotification = get_video_comment_notification_model()
+
+
+class VideoManager(models.Manager):
+        def create(self, *args, **kwargs):
+            video =  super(VideoManager, self).create(*args, **kwargs)
+
+            if kwargs['answer_to']:
+                # create VideoCommentNotification if video is a comment
+                VideoCommentNotification.objects.create(
+                    owner_id=answer_to.id,
+                    video_comment=video
+                )
+
+            return video
 
 
 class Video(models.Model):
@@ -14,3 +29,8 @@ class Video(models.Model):
     file = models.FileField(upload_to='videos', blank=False, null=True,)
     resources_link = models.URLField()
     author = models.ForeignKey(User, related_name='videos', on_delete=models.CASCADE)
+
+    # If video is a comment, this field represents the video it is commenting on.
+    answer_to = models.ForeignKey(Video, related_name='comments', on_delete=models.CASCADE)
+
+    objects = VideoManager()
