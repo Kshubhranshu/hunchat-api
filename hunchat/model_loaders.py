50469ba9cf5ecf407eef6517d6776aaabb570847
Ernesto F. González <ernestogonzalezz@yahoo.com>
2020-12-09 23:41:06 +0000

    :sparkles: create model loaders

---
+++ hunchat/model_loaders.py
@@ -0,0 +1,9 @@
+from django.apps import apps
+
+
+def get_video_comment_notification_model():
+    return app.get_model('notifications.VideoCommentNotification')
+
+
+def get_video_model():
+    return apps.get_model('videos.Video')
