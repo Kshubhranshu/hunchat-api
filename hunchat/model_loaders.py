647100d3ba4b39afd0ef266f33a3f1a80c71a28f
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-01-27 22:33:52 +0000

    :bug: :pencil2: fix typo in model_loaders.get_video_comment_notification_model

--- hunchat/model_loaders.py
+++ hunchat/model_loaders.py
@@ -2,7 +2,7 @@ from django.apps import apps
 
 
 def get_video_comment_notification_model():
-    return app.get_model("notifications.VideoCommentNotification")
+    return apps.get_model("notifications.VideoCommentNotification")
 
 
 def get_video_model():
