a1f1f018725f809e72408a148ffd7d2de682e853
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-05 15:31:42 +0000

    :rocket: Launch to Heroku

--- hunchat/settings.py
+++ hunchat/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
+import django_heroku
 
 
 # Load environment variables from .env file
@@ -117,11 +119,11 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": str(BASE_DIR / "db.sqlite3"),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 AUTH_USER_MODEL = "authentication.User"
