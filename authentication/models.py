a6ed5fd6f6c8581885bf5f1eab6151938fe4296a
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-11 12:13:19 +0000

    :card_file_box: update User model

--- authentication/models.py
+++ authentication/models.py
@@ -7,16 +7,46 @@ class User(AbstractUser):
     Custom User model.
     """
 
+    first_name = None
+    last_name = None
+    name = models.CharField(null=False, blank=False, max_length=50)
+
     email = models.EmailField(unique=True, null=False, blank=False)
     is_email_verified = models.BooleanField(default=False)
 
     # profile image
-    image = models.ImageField(upload_to="images")
+    image = models.ImageField(upload_to="images", null=True)
 
-    # Bio video
-    # bio = models.ForeignKey()
+    bio = models.CharField(null=True, blank=True, max_length=200)
+    bio_video = models.ForeignKey(
+        "videos.Video",
+        related_name="user",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+
+    location = models.CharField(null=True, blank=True, max_length=40)
+
+    link = models.URLField(null=True, blank=True)
 
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = [
+        "name",
         "email",
     ]
+
+    @classmethod
+    def is_username_taken(cls, username):
+        users = cls.objects.filter(username=username)
+        if not users.exists():
+            return False
+        return True
+
+    @classmethod
+    def is_email_taken(cls, email):
+        try:
+            cls.objects.get(email=email)
+            return True
+        except User.DoesNotExist:
+            return False
