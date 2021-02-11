4575e35d929a7673c2911e1d7d783694b963b2ad
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-11 12:14:17 +0000

    :sparkles: set HASH_IDS

--- authentication/__init__.py
+++ authentication/__init__.py
@@ -1,1 +1,8 @@
+from django.conf import settings
+
+import hashids
+
+
 default_app_config = "authentication.apps.AuthenticationConfig"
+
+HASH_IDS = hashids.Hashids(salt=settings.HASHID_SALT, min_length=12)
