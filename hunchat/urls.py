7a558cf0d71175214097bfb7f2e2e65877d383a0
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-11 12:19:17 +0000

    :sparkles: include authentication.urls in project urls

--- hunchat/urls.py
+++ hunchat/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include("authentication.urls")),
     path("api/invitations/", include("invitations.urls")),
 ]
