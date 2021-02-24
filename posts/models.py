59d3cc6ca7a7f90eb3621087820fc4d9de02efc0
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-24 11:15:34 +0000

    :sparkles: create Post methods and PostLike model

--- posts/models.py
+++ posts/models.py
@@ -1,19 +1,7 @@
 from django.db import models
 from django.conf import settings
 
-
-class PostManager(models.Manager):
-    def create(self, *args, **kwargs):
-        post = super(PostManager, self).create(*args, **kwargs)
-
-        if kwargs["answer_to"]:
-            # create PostCommentNotification if post is a comment
-            PostCommentNotification = get_post_comment_notification_model()
-            PostCommentNotification.objects.create(
-                owner_id=answer_to.id, post_comment=post
-            )
-
-        return post
+from posts.managers import PostManager, PostLikeManager
 
 
 class Post(models.Model):
@@ -27,8 +15,56 @@ class Post(models.Model):
     author = models.ForeignKey(
         "authentication.User", related_name="posts", on_delete=models.CASCADE
     )
-    answer_to = models.ForeignKey(
-        "posts.Post", related_name="comments", on_delete=models.CASCADE
+    created_at = models.DateTimeField(auto_now_add=True)
+    comment_to = models.ForeignKey(
+        "posts.Post",
+        related_name="comments",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
     )
 
     objects = PostManager()
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def get_comments(self):
+        return self.comments
+
+    def get_comments_count(self):
+        return self.get_comments().count()
+
+    def get_likes(self):
+        return self.likes
+
+    def get_likes_count(self):
+        return self.get_likes().count()
+
+    def get_thread(self):
+        if self.comment_to:
+            thread = [self]
+            next_post_up = self.comment_to
+            while next_post_up:
+                thread.insert(0, next_post_up)
+                next_post_up = next_post_up.comment_to
+            return thread
+        return None
+
+
+class PostLike(models.Model):
+    user = models.ForeignKey(
+        "authentication.User", related_name="likes", on_delete=models.CASCADE
+    )
+    post = models.ForeignKey(
+        "posts.Post",
+        related_name="likes",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+
+    objects = PostLikeManager()
+
+    class Meta:
+        unique_together = ("user", "post")
