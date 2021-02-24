b53d92b819f7d5014e17bdda30ed20deba3bbaa1
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-24 11:12:12 +0000

    :sparkles: create new model loaders

--- hunchat/model_loaders.py
+++ hunchat/model_loaders.py
@@ -1,12 +1,24 @@
 from django.apps import apps
 
 
+def get_notification_model():
+    return apps.get_model("notifications.Notification")
+
+
 def get_post_comment_notification_model():
     return apps.get_model("notifications.PostCommentNotification")
 
 
+def get_post_like_notification_model():
+    return apps.get_model("notifications.PostLikeNotification")
+
+
 def get_post_model():
     return apps.get_model("posts.Post")
+
+
+def get_post_like_model():
+    return apps.get_model("posts.PostLike")
 
 
 def get_video_model():
