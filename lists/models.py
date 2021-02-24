74fd7a6683887e149ce12c67285489b8db94957d
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-24 11:14:22 +0000

    :construction: update lists

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class List(models.Model):
+    name = models.CharField(max_length=20)
+    tag = models.SlugField()
+    owner = models.ForeignKey(
+        "authentication.User", related_name="lists", on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
