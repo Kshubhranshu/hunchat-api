ca4ecbbdced486e1b8679337192636cf590501ab
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-03-14 22:00:39 +0000

    :sparkles: update VideoSerializer

--- videos/serializers.py
+++ videos/serializers.py
@@ -5,13 +5,31 @@ from hunchat.model_loaders import get_video_model
 
 class VideoSerializer(serializers.ModelSerializer):
     file_url = serializers.SerializerMethodField()
+    poster_url = serializers.SerializerMethodField()
 
     class Meta:
         model = get_video_model()
-        fields = ["id", "file", "file_url", "duration", "height", "width"]
+        fields = [
+            "id",
+            "file",
+            "file_url",
+            "duration", 
+            "height",
+            "width",
+            "poster",
+            "poster_url",
+            "poster_height",
+            "poster_width",
+        ]
 
     def get_file_url(self, obj):
         file = obj.file
         if file:
             return file.url
+        return None
+
+    def get_poster_url(self, obj):
+        poster = obj.poster
+        if poster:
+            return poster.url
         return None
