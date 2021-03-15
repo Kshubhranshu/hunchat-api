5046545d0632c8cc4922791bd90e3df0d1213309
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-03-15 21:57:44 +0000

    :sparkles: update PostCommentNotificationSerializer and NotificationSerializer

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -2,44 +2,41 @@ from django.utils.translation import gettext_lazy as _
 
 from rest_framework import serializers
 
+from rest_framework_serializer_extensions.serializers import SerializerExtensionsMixin
 from rest_framework_serializer_extensions.fields import HashIdField
 
-from notifications.models import (
-    Notification,
-    PostCommentNotification,
-    PostLikeNotification,
+from generic_relations.relations import GenericRelatedField
+
+from hunchat.model_loaders import (
+    get_notification_model,
+    get_post_comment_notification_model,
+    get_post_like_notification_model,
 )
 
 from posts.serializers import PostCommentSerializer, PostLikeSerializer
 
 
-class NotificationContentObjectRelatedField(serializers.RelatedField):
-    """
-    A custom field to use for the `content_object` generic relationship.
-    """
-
-    def to_representation(self, value):
-        """
-        Serializer notification instances using specific notification type serializers.
-        """
-        if isinstance(value, PostCommentNotification):
-            serializer = PostCommentSerializer(value)
-            return serializer.data
-        elif isinstance(value, PostLikeNotification):
-            serializer = PostLikeSerializer(value)
-            return serializer.data
-        else:
-            raise Exception(_("Unexpected type of notification object."))
-
-
-class NotificationSerializer(serializers.ModelSerializer):
-    """
-    Notification serializer.
-    """
-
-    id = HashIdField(model=Notification, read_only=True)
-    content_object = NotificationContentObjectRelatedField(read_only=True)
+class PostCommentNotificationSerializer(serializers.ModelSerializer):
+    post_comment = PostCommentSerializer()
 
     class Meta:
-        model = Notification
+        model = get_post_comment_notification_model()
+        fields = ["post_comment"]
+
+
+class NotificationSerializer(SerializerExtensionsMixin, serializers.ModelSerializer):
+    """
+    Notification serializer with a `GenericRelatedField` mapping all possible
+    notifications models to their respective serializers..
+    """
+
+    id = HashIdField(model=get_notification_model(), read_only=True)
+    content_object = GenericRelatedField(
+        {
+            get_post_comment_notification_model(): PostCommentNotificationSerializer(),
+        }
+    )
+
+    class Meta:
+        model = get_notification_model()
         fields = ["id", "notification_type", "created_at", "unread", "content_object"]
