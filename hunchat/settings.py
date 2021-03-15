3ec8ff65ce3e2f076e79d717eef8ba83c3eeb356
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-03-15 21:54:49 +0000

    :wrench::construction: add generic_relations and django_rq to INSTALLED_APPS; start django_rq settings

--- hunchat/settings.py
+++ hunchat/settings.py
@@ -64,8 +64,10 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt.token_blacklist",
     "rest_framework_serializer_extensions",
     "drf_yasg",
+    "generic_relations",
     "corsheaders",
     "storages",
+    "django_rq",
     "authentication",
     "invitations",
     "lists",
@@ -174,6 +176,39 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+
+# Queues
+# https://github.com/rq/django-rq
+
+RQ_QUEUES = {
+    "default": {
+        "HOST": "localhost",
+        "PORT": 6379,
+        "DB": 0,
+        "PASSWORD": "some-password",
+        "DEFAULT_TIMEOUT": 360,
+    },
+    "with-sentinel": {
+        "SENTINELS": [("localhost", 26736), ("localhost", 26737)],
+        "MASTER_NAME": "redismaster",
+        "DB": 0,
+        "PASSWORD": "secret",
+        "SOCKET_TIMEOUT": None,
+        "CONNECTION_KWARGS": {"socket_connect_timeout": 0.3},
+    },
+    "high": {
+        "URL": os.getenv(
+            "REDISTOGO_URL", "redis://localhost:6379/0"
+        ),  # If you're on Heroku
+        "DEFAULT_TIMEOUT": 500,
+    },
+    "low": {
+        "HOST": "localhost",
+        "PORT": 6379,
+        "DB": 0,
+    },
+}
 
 
 # Internationalization
