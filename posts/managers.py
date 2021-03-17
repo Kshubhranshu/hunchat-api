d926eaacb30d1923aa6236b3980a7d93fe9a1bca
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-03-17 00:29:06 +0000

    :sparkles: update PostManager and PostLikeManager

--- posts/managers.py
+++ posts/managers.py
@@ -13,23 +13,27 @@ class PostManager(models.Manager):
         comment_to = kwargs.get("comment_to", None)
         if comment_to:
             # create PostCommentNotification if post is a comment
-            PostCommentNotification = get_post_comment_notification_model()
-            PostCommentNotification.objects.create(
-                owner_id=comment_to.author.id, post_comment=post
-            )
+            if post.author.id != comment_to.author.id:
+                # check if post and comment authors are different
+                PostCommentNotification = get_post_comment_notification_model()
+                PostCommentNotification.objects.create(
+                    owner_id=comment_to.author.id, post_comment=post
+                )
 
         return post
 
 
 class PostLikeManager(models.Manager):
     def create(self, *args, **kwargs):
-        post_like = super(PostManager, self).create(*args, **kwargs)
+        post_like = super(PostLikeManager, self).create(*args, **kwargs)
 
         post_author = kwargs.get("post").author
 
-        PostLikeNotification = get_post_like_notification_model()
-        PostLikeNotification.objects.create(
-            owner_id=post_author.id, post_like=post_like
-        )
+        if post_like.user != post_author:
+            # check if user who liked is different than post's author
+            PostLikeNotification = get_post_like_notification_model()
+            PostLikeNotification.objects.create(
+                owner_id=post_author.id, post_like=post_like
+            )
 
         return post_like
