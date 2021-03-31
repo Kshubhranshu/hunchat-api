d9c2c4e6ae00353d33b04d65197ab0808598f075
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-03-31 23:30:36 +0100

    :white_check_mark: add tests for User

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework.test import APITestCase, URLPatternsTestCase
+
+from authentication.models import User
+
+
+class UserModelTests(TestCase):
+    # fixtures = ["users.json"]
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(
+            name="Charles Darwin", username="charles", email="charles@hmsbeagle.com"
+        )
+
+    def test_is_username_taken(self):
+        self.assertIs(User.is_username_taken("charles"), True)
+        self.assertIs(User.is_username_taken("marie"), False)
