b8bf6a9f9ef218128cfce51101481ecb43c7720b
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-04-22 22:58:52 +0100

    :sparkles: add get_video_file_path and get_image_file_path methods

--- hunchat/storage.py
+++ hunchat/storage.py
@@ -1,3 +1,6 @@
+import uuid
+import os
+
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
@@ -16,3 +19,15 @@ class S3MediaStorage(S3Boto3Storage):
     """
 
     location = settings.AWS_MEDIA_LOCATION
+
+
+def get_video_file_path(instance, filename):
+    ext = filename.split(".")[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join("videos/", filename)
+
+
+def get_image_file_path(instance, filename):
+    ext = filename.split(".")[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join("images/", filename)
