ff9572e5cd5ab400c75213edb46b6a40113749f9
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-05-26 22:41:13 +0100

    :wrench: set ACCESS_TOKEN_LIFETIME to 10 minutes

--- hunchat/settings.py
+++ hunchat/settings.py
@@ -141,7 +141,7 @@ REST_FRAMEWORK = {
 HASHID_SALT = os.environ.get("HASHID_SALT")
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=86400), # 60 days (change to 5 minutes)
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": False,
